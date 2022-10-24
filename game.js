420dead941b81029dfae8035031d5b9b21c91865
uahstudent <113307242+uahstudent@users.noreply.github.com>
2022-10-24 16:38:51 -0500

    Create game.js

---
+++ game.js
@@ -0,0 +1,26 @@
+var player
+var otherPlayer
+
+function placeInSpace(space) {
+    document.getElementById(space).innerHTML = player;
+}
+
+function isWinner() {
+    if (document.getElementById("00").innerHTML.trim() === player && document.getElementById("01").innerHTML.trim() === player && document.getElementById("02").innerHTML.trim() === player) {
+        alert("Winner!")
+    } else if (document.getElementById("10").innerHTML.trim() === player && document.getElementById("11").innerHTML.trim() === player && document.getElementById("12").innerHTML.trim() === player) {
+        alert("Winner!")
+    } else if (document.getElementById("20").innerHTML.trim() === player && document.getElementById("21").innerHTML.trim() === player && document.getElementById("22").innerHTML.trim() === player) {
+        alert("Winner!")
+    } else if (document.getElementById("00").innerHTML.trim() === player && document.getElementById("10").innerHTML.trim() === player && document.getElementById("20").innerHTML.trim() === player) {
+        alert("Winner!")
+    } else if (document.getElementById("01").innerHTML.trim() === player && document.getElementById("11").innerHTML.trim() === player && document.getElementById("21").innerHTML.trim() === player) {
+        alert("Winner!")
+    } else if (document.getElementById("02").innerHTML.trim() === player && document.getElementById("12").innerHTML.trim() === player && document.getElementById("22").innerHTML.trim() === player) {
+        alert("Winner!")
+    } else if (document.getElementById("00").innerHTML.trim() === player && document.getElementById("11").innerHTML.trim() === player && document.getElementById("22").innerHTML.trim() === player) {
+        alert("Winner!")
+    } else if (document.getElementById("02").innerHTML.trim() === player && document.getElementById("11").innerHTML.trim() === player && document.getElementById("20").innerHTML.trim() === player) {
+        alert("Winner!")
+    }
+}
