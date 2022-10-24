ab68275e0623b9edd2c642ac3d54e098cf61134e
uahstudent <113307242+uahstudent@users.noreply.github.com>
2022-10-24 16:48:42 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,32 +1,41 @@
 var player
 var otherPlayer
+var isTurn
 
 function setPlayerSymbols() {
 
 }
 
 function placeInSpace(space) {
-    document.getElementById(space).innerHTML = player;
+    if (isTurn) {
+        document.getElementById(space).innerHTML = player;
+    }
 }
 
 function isWinner() {
     if (document.getElementById("00").innerHTML.trim() === player && document.getElementById("01").innerHTML.trim() === player && document.getElementById("02").innerHTML.trim() === player) {
+        isTurn = false
         alert("Winner!")
     } else if (document.getElementById("10").innerHTML.trim() === player && document.getElementById("11").innerHTML.trim() === player && document.getElementById("12").innerHTML.trim() === player) {
+        isTurn = false
         alert("Winner!")
     } else if (document.getElementById("20").innerHTML.trim() === player && document.getElementById("21").innerHTML.trim() === player && document.getElementById("22").innerHTML.trim() === player) {
+        isTurn = false
         alert("Winner!")
     } else if (document.getElementById("00").innerHTML.trim() === player && document.getElementById("10").innerHTML.trim() === player && document.getElementById("20").innerHTML.trim() === player) {
+        isTurn = false
         alert("Winner!")
     } else if (document.getElementById("01").innerHTML.trim() === player && document.getElementById("11").innerHTML.trim() === player && document.getElementById("21").innerHTML.trim() === player) {
+        isTurn = false
         alert("Winner!")
     } else if (document.getElementById("02").innerHTML.trim() === player && document.getElementById("12").innerHTML.trim() === player && document.getElementById("22").innerHTML.trim() === player) {
+        isTurn = false
         alert("Winner!")
     } else if (document.getElementById("00").innerHTML.trim() === player && document.getElementById("11").innerHTML.trim() === player && document.getElementById("22").innerHTML.trim() === player) {
+        isTurn = false
         alert("Winner!")
     } else if (document.getElementById("02").innerHTML.trim() === player && document.getElementById("11").innerHTML.trim() === player && document.getElementById("20").innerHTML.trim() === player) {
+        isTurn = false
         alert("Winner!")
     }
 }
-
-
