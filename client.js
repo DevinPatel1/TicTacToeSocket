be79950115d15dc5f44676d6f264599e05f66398
Devin Patel <devinpatel02@gmail.com>
2022-11-01 15:35:19 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,3 +1,17 @@
+/*
+* Project:        Tic Tac Toe Socket Project
+* Course:         CS 370 - 01
+* Term:           FA 22
+* Team #:         2
+* Team Members:   Devin Patel
+*                 Jainum Patel
+*                 Faith Grimmeisen
+*
+* Purpose: To reinforce the concepts covered throughout
+* the course by creating a client-server socket application.
+* 
+*/
+
 import net from "net"
 import readline from "readline"
 
